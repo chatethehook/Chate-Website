cf128f35f03a9e2da3aecdac96b66f03ed4d5a0f
salamijason <oattrakyaw@gmail.com>
2024-04-17 22:06:30 +0630

    js header testing

--- src/HeaderFooter.js
+++ src/HeaderFooter.js
@@ -1,6 +1,32 @@
 class SpecialHeader extends HTMLElement {
     ConnectedCallBack () {
         this.innerHTML = `
+        <nav class="navbar navbar-expand-sm bg-body sticky-top">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse justify-content-center"
+            id="navbarNavAltMarkup"
+          >
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
+              <a class="nav-link" href="/articles.html">Articles</a>
+
+              <a class="nav-link" href="/events.html">Events</a>
+            </div>
+          </div>
+        </div>
+        </nav>
         `
         
     }
