876629fc1025630dd442ae7ddff5b042852f55a0
salamijason <oattrakyaw@gmail.com>
2024-04-17 23:06:16 +0630

    HeaderTweaks

--- src/HeaderFooter.js
+++ src/HeaderFooter.js
@@ -1,17 +1,65 @@
+alert(window.location.hostname)
 class SpecialHeader extends HTMLElement {
-    connectedCallBack () {
-        this.innerHTML = `
-            <p>test</p>
-        `
+    connectedCallback () {
+        if (window.location.hostname == 'www.chatethehook.netlify.app') {
+            this.innerHTML = `
+            <nav class="navbar navbar-expand-sm bg-body sticky-top">
+                <div class="container-fluid">
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    class="collapse navbar-collapse justify-content-center"
+                    id="navbarNavAltMarkup"
+                >
+                    <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    <a class="nav-link" href="/articles.html">Articles</a>
+
+                    <a class="nav-link" href="/events.html">Events</a>
+                    
+                    </div>
+                </div>
+                </div>
+            </nav>
+            `
+        }
         
     }
 }
 class SpecialFooter extends HTMLElement {
-    connectedCallBack () {
+    connectedCallback () {
         this.innerHTML = `
+            <div class="footer">
+                <br />
+                <p>
+                    <a
+                        class="primary"
+                        href=""
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >Chate</a
+                    >
+                    source code
+                    <a
+                        href="https://github.com/salamijason/chate-website"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >github</a
+                    >
+                </p>
+            </div>
         `
         
     }
 }
-customElements.define('special-header',SpecialHeader)
-customElements.define('special-footer',SpecialFooter)
+customElements.define('special-header',SpecialHeader);
+customElements.define('special-footer',SpecialFooter);
