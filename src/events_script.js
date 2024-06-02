d828d3ba2709fee30b93fe51eef8acd502f7b1c5
salamijason <oattrakyaw@gmail.com>
2024-06-02 23:38:55 +0630

    iframe fixes

--- src/events_script.js
+++ src/events_script.js
@@ -10,9 +10,10 @@ let pastContent = `
         </h1>
     </div>
     <div class="row p-2">
-        <div class="col-sm-3">
+        <div class="col-3">
         <iframe
             src="https://www.youtube.com/embed/sxHyH7kSoNU?si=gBx4uXoWJqyNnniA"
+            class="responsive-iframe"
         >
         </iframe>
         <h2 class="m-0">
@@ -25,9 +26,10 @@ let pastContent = `
         <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 4</p>
         
         </div>
-        <div class="col-sm-3">
+        <div class="col-3">
         <iframe
             src="https://www.youtube.com/embed/YOwYQ56EZaA?si=1nCk3neoXNqL4by9"
+            class="responsive-iframe"
         >
         </iframe>
         <h2 class="m-0">
@@ -39,9 +41,10 @@ let pastContent = `
         <p class="textorange m-0">Yin Min Thant</p>
         <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 3</p>
         </div>
-        <div class="col-sm-3">
+        <div class="col-3">
         <iframe
             src="https://www.youtube.com/embed/HRfzWAFDMQk?si=Yg7OvPHK8tSN_lju"
+            class="responsive-iframe"
         >
         </iframe>
         <h2 class="m-0">
@@ -53,7 +56,7 @@ let pastContent = `
         <p class="textorange m-0">Sai Nyi Bhone Htut, Myat Nadi Kyaw</p>
         <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 2</p>
         </div>
-        <div class="col-sm-3">
+        <div class="col-3">
         <img
             src="media/webinar resources/nlhfirstwebinar.png"
             alt="webinar thumbnail"
@@ -81,7 +84,7 @@ let newContent = `
         </h1>
       </div>
       <div class="row p-2">
-        <div class="col-sm-3">
+        <div class="col-3">
           <img
                         src="media/webinar resources/Zoom_background.png"
                         alt="webinar thumbnail"
