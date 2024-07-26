de6f2f3be2daf5f31944498ccf13bf0cbca628bf
Thu San Htet <167324089+mgthusanhtet@users.noreply.github.com>
2024-07-26 18:54:05 +0700

    Add files via upload

--- src/events_script.js
+++ src/events_script.js
@@ -11,11 +11,12 @@ let pastContent =
     </div>
     <div class="row p-2">
       <div class="col-3 pb-3">
-          <img
-                        src="media/webinar resources/Zoom_background.png"
-                        alt="webinar thumbnail"
-                        class="img-fluid rounded pb-2"
-                    />
+          <iframe src="https://www.youtube.com/embed/xn7rv-xiWAM?si=F3xTD-WwxZeaaeui" 
+            class="responsive-iframe" 
+            title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+          </iframe>
           <h2 class="m-0">
             Fully Funded Undergraduate Scholarship in the US with a GED&nbsp;
             <span class="textaccent"
@@ -26,11 +27,12 @@ let pastContent =
           <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 10</p>
       </div>
       <div class="col-3 pb-3">
-          <img
-                        src="media/webinar resources/Zoom_background.png"
-                        alt="webinar thumbnail"
-                        class="img-fluid rounded pb-2"
-                    />
+          <iframe src="https://www.youtube.com/embed/C60d95ecPMg?si=mHv0uHckFzfwtFsD" 
+            class="responsive-iframe" 
+            title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+          </iframe>
           <h2 class="m-0">
             Doing the Best with What I Have&nbsp;
             <span class="textaccent"
@@ -41,10 +43,11 @@ let pastContent =
           <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 9</p>
       </div>
       <div class="col-3 pb-3">
-          <iframe
-            src="https://www.youtube.com/embed/I-2qd55aBeA?si=S866eQe4PZDAxCO0"
-            class="responsive-iframe"
-          >
+          <iframe src="https://www.youtube.com/embed/I-2qd55aBeA?si=S866eQe4PZDAxCO0" 
+            class="responsive-iframe" 
+            title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
           </iframe>
           <h2 class="m-0">
             Extracurricular Activities for Colleges&nbsp;
@@ -56,57 +59,60 @@ let pastContent =
           <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 8</p>
         </div>
       <div class="col-3 pb-3">
-          <iframe
-            src="https://www.youtube.com/embed/sNl1rv3X39k?si=G72gCtCq4qutrH0q"
-            class="responsive-iframe"
-        >
+        <iframe src="https://www.youtube.com/embed/sNl1rv3X39k?si=G72gCtCq4qutrH0q" 
+            class="responsive-iframe" 
+            title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
         </iframe>
-          <h2 class="m-0">
-            Financial Aid at US Colleges&nbsp;
-            <span class="textaccent"
-            ><p class="d-inline m-0 text-right">23/06/2024</p></span
-            >
-          </h2>
-          <p class="textorange m-0">Wai Yan Wing Aung</p>
-          <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 7</p>
-        </div>
-        <div class="col-3 pb-3">
-          <iframe
-            src="https://www.youtube.com/embed/pLIlUeuaELw?si=8Mu7mfcsoH7Z6XgK"
-            class="responsive-iframe"
-        >
+        <h2 class="m-0">
+          Financial Aid at US Colleges&nbsp;
+          <span class="textaccent"
+          ><p class="d-inline m-0 text-right">23/06/2024</p></span
+          >
+        </h2>
+        <p class="textorange m-0">Wai Yan Wing Aung</p>
+        <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 7</p>
+      </div>
+      <div class="col-3 pb-3">   
+        <iframe src="https://www.youtube.com/embed/pLIlUeuaELw?si=8Mu7mfcsoH7Z6XgK" 
+            class="responsive-iframe" 
+            title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
         </iframe>
-          <h2 class="m-0">
-            "တက္ကသိုလ်များဆီသို့" Program မိတ်ဆက်&nbsp;
-            <span class="textaccent"
-            ><p class="d-inline m-0 text-right">16/06/2024</p></span
-            >
-          </h2>
-          <p class="textorange m-0">ချိတ် Admissions Officers</p>
-          <p class="m-1">Official Program Announcement</p>
-          <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 6</p>
+        <h2 class="m-0">
+          "တက္ကသိုလ်များဆီသို့" Program မိတ်ဆက်&nbsp;
+          <span class="textaccent"
+          ><p class="d-inline m-0 text-right">16/06/2024</p></span
+          >
+        </h2>
+        <p class="textorange m-0">ချိတ် Admissions Officers</p>
+        <p class="m-1">Official Program Announcement</p>
+        <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 6</p>
 
-        </div>
-        <div class="col-3 pb-3">
-          <img
-                        src="media/webinar resources/whatarecollegeslookingfor.jpg"
-                        alt="webinar thumbnail"
-                        class="img-fluid rounded pb-2"
-                    />
-          <h2 class="m-0">
-            What Are Top Colleges Looking For?&nbsp;
-            <span class="textaccent"
-            ><p class="d-inline m-0 text-right">02/06/2024</p></span
-            >
-          </h2>
-          <p class="textorange m-0">Min Thaik Aung Saw</p>
-          <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 5</p>
-        </div>
-        <div class="col-3 pb-3">
-        <iframe
-            src="https://www.youtube.com/embed/sxHyH7kSoNU?si=gBx4uXoWJqyNnniA"
-            class="responsive-iframe"
-        >
+      </div>
+      <div class="col-3 pb-3">
+        <img
+          src="media/webinar resources/whatarecollegeslookingfor.jpg"
+          alt="webinar thumbnail"
+          class="img-fluid rounded pb-2"
+        />
+        <h2 class="m-0">
+          What Are Top Colleges Looking For?&nbsp;
+          <span class="textaccent"
+          ><p class="d-inline m-0 text-right">02/06/2024</p></span
+          >
+        </h2>
+        <p class="textorange m-0">Min Thaik Aung Saw</p>
+        <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 5</p>
+      </div>
+      <div class="col-3 pb-3">
+        <iframe src="https://www.youtube.com/embed/sxHyH7kSoNU?si=gBx4uXoWJqyNnniA" 
+            class="responsive-iframe" 
+            title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
         </iframe>
         <h2 class="m-0">
             My Journey from Community College to Columbia University&nbsp;
@@ -116,13 +122,14 @@ let pastContent =
         </h2>
         <p class="textorange m-0">Rachel Soe</p>
         <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 4</p>
-        
-        </div>
-        <div class="col-3 pb-3">
-        <iframe
-            src="https://www.youtube.com/embed/YOwYQ56EZaA?si=1nCk3neoXNqL4by9"
-            class="responsive-iframe"
-        >
+      
+      </div>
+      <div class="col-3 pb-3">
+        <iframe src="https://www.youtube.com/embed/YOwYQ56EZaA?si=1nCk3neoXNqL4by9" 
+            class="responsive-iframe" 
+            title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
         </iframe>
         <h2 class="m-0">
             Misconceptions in College Applications&nbsp;
@@ -132,12 +139,13 @@ let pastContent =
         </h2>
         <p class="textorange m-0">Yin Min Thant</p>
         <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 3</p>
-        </div>
-        <div class="col-3 pb-3">
-        <iframe
-            src="https://www.youtube.com/embed/HRfzWAFDMQk?si=Yg7OvPHK8tSN_lju"
-            class="responsive-iframe"
-        >
+      </div>
+      <div class="col-3 pb-3">
+        <iframe src="https://www.youtube.com/embed/HRfzWAFDMQk?si=Yg7OvPHK8tSN_lju" 
+            class="responsive-iframe" 
+            title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
         </iframe>
         <h2 class="m-0">
             Why LACs?&nbsp;
@@ -147,8 +155,8 @@ let pastContent =
         </h2>
         <p class="textorange m-0">Sai Nyi Bhone Htut, Myat Nadi Kyaw</p>
         <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 2</p>
-        </div>
-        <div class="col-3 pb-3">
+      </div>
+      <div class="col-3 pb-3">
         <img
             src="media/webinar resources/ASK THE HOOK.png"
             alt="webinar thumbnail"
@@ -162,7 +170,7 @@ let pastContent =
         </h2>
         <p class="textorange m-0">Nyan Lin Htet</p>
         <p class="m-1">"ကျောင်းလျှောက်ကြမယ်" Webinar Series, Episode 1</p>
-        </div>
+      </div>
     </div>
 `;
 let newContent = `
