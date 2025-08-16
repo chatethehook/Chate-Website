fb1c2ae42b2b8d3b83e29eaca48f0a12c30d01e8
salamijason <oattrakyaw@gmail.com>
2025-08-16 16:54:13 +0630

    Revert "Merge branch 'new-video-tab' into main"
    This reverts commit cc45ff3b5bfc836c8e95e34b2209eeddb0287f1a, reversing
    changes made to d23dac5563f1782b8c0efaff60953aa0b88c2760.

--- src/events_script.js
+++ src/events_script.js
@@ -743,11 +743,171 @@ let kyaungShautContent = `
 
   </div>
 `;
+let suTuPyuContent = `
+  <div
+  class="justify-content-center p-2 d-flex"
+  style="padding-right: 50px"
+  >
+    <h1 class="">
+      စုတုပြု
+      <span class="textaccent text-center">Video Series</span>
+    </h1>
+  </div>
+
+  <div class="row p-2">
+
+    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 pb-3">
+      <iframe src="https://www.youtube.com/embed/DQo_3gRPBu0?si=P7JHwilIKUcd6M3D"
+            class="responsive-iframe"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+      </iframe>
+      <h2 class="m-0">
+        Brown University Accepted Essay&nbsp;
+        <span class="textaccent"
+        ><p class="d-inline m-0 text-right">06/03/2025</p></span
+        >
+      </h2>
+      <p class="textorange m-0">Written by <span class="fw-bold">Swan Tayza Aung</span></p>
+      <p class="m-1">Read by Phyu Thant Kyi</p>
+      <p class="m-1">"စုတုပြု" Video Series, Episode 8</p>
+    </div>
+
+    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 pb-3">
+      <iframe src="https://www.youtube.com/embed/dWnR9Bos4jU?si=yAE0ymj_POivO9HW"
+            class="responsive-iframe"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+      </iframe>
+      <h2 class="m-0">
+        Occidental College Accepted Essay&nbsp;
+        <span class="textaccent"
+        ><p class="d-inline m-0 text-right">26/01/2025</p></span
+        >
+      </h2>
+      <p class="textorange m-0">Written by <span class="fw-bold">Aung Myat Htet</span></p>
+      <p class="m-1">Read by Phyu Thant Kyi</p>
+      <p class="m-1">"စုတုပြု" Video Series, Episode 7</p>
+    </div>
+
+    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 pb-3">
+      <iframe src="https://www.youtube.com/embed/T-snQV67LdQ?si=_AE1UEO-BaFclAY5"
+            class="responsive-iframe"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+      </iframe>
+      <h2 class="m-0">
+        Vanderbilt University Accepted Essay&nbsp;
+        <span class="textaccent"
+        ><p class="d-inline m-0 text-right">06/01/2025</p></span
+        >
+      </h2>
+      <p class="textorange m-0">Written by <span class="fw-bold">Sai Nyi Bhone Htut</span></p>
+      <p class="m-1">Read by Phyu Thant Kyi</p>
+      <p class="m-1">"စုတုပြု" Video Series, Episode 6</p>
+    </div>
+
+    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 pb-3">
+      <iframe src="https://www.youtube.com/embed/4daBKoIkShM?si=Tp6l4ArCM6xe6SAq"
+            class="responsive-iframe"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+      </iframe>
+      <h2 class="m-0">
+        Rice University Accepted Essay&nbsp;
+        <span class="textaccent"
+        ><p class="d-inline m-0 text-right">03/11/2024</p></span
+        >
+      </h2>
+      <p class="textorange m-0">Written by <span class="fw-bold">Yin Min Thant</span></p>
+      <p class="m-1">Read by Phyu Thant Kyi</p>
+      <p class="m-1">"စုတုပြု" Video Series, Episode 5</p>
+    </div>
+
+    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 pb-3">
+      <iframe src="https://www.youtube.com/embed/yZzQSwoaYOg?si=AIOPynj0S1krWkpW"
+            class="responsive-iframe"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+      </iframe>
+      <h2 class="m-0">
+        Brown University Accepted Essay&nbsp;
+        <span class="textaccent"
+        ><p class="d-inline m-0 text-right">02/11/2024</p></span
+        >
+      </h2>
+      <p class="textorange m-0">Written by <span class="fw-bold">Yone Waddy Aung</span></p>
+      <p class="m-1">Read by Myat Hmu Thwin</p>
+      <p class="m-1">"စုတုပြု" Video Series, Episode 4</p>
+    </div>
+
+    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 pb-3">
+      <iframe src="https://www.youtube.com/embed/BVY2fMzcBvo?si=8BNFODatn4jB2DPm"
+            class="responsive-iframe"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+      </iframe>
+      <h2 class="m-0">
+        2024 Swarthmore Accepted Essay&nbsp;
+        <span class="textaccent"
+        ><p class="d-inline m-0 text-right">08/10/2024</p></span
+        >
+      </h2>
+      <p class="textorange m-0">Written by <span class="fw-bold">Sai Nyi Bhone Htut</span></p>
+      <p class="m-1">Read by Phyu Thant Kyi</p>
+      <p class="m-1">"စုတုပြု" Video Series, Episode 3</p>
+    </div>
+
+    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 pb-3">
+      <iframe src="https://www.youtube.com/embed/vbB0sdRjTQ4?si=8RONA5K3fRMd4ueu"
+            class="responsive-iframe"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+      </iframe>
+      <h2 class="m-0">
+        2024 Dartmouth Accepted Essay&nbsp;
+        <span class="textaccent"
+        ><p class="d-inline m-0 text-right">16/09/2024</p></span
+        >
+      </h2>
+      <p class="textorange m-0">Written by <span class="fw-bold">Wai Yan Win Aung</span></p>
+      <p class="m-1">Read by Myat Hmu Thwin</p>
+      <p class="m-1">"စုတုပြု" Video Series, Episode 2</p>
+    </div>
+
+    <div class=" col-12 col-sm-6 col-lg-4 col-xl-3 pb-3">
+      <iframe src="https://www.youtube.com/embed/DMwt8OBGQSM?si=hB4YU6UHSmIJIRNZ"
+            class="responsive-iframe"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+      </iframe>
+      <h2 class="m-0">
+        2024 MIT Accepted Essay&nbsp;
+        <span class="textaccent"
+        ><p class="d-inline m-0 text-right">13/08/2024</p></span
+        >
+      </h2>
+      <p class="textorange m-0">Written by <span class="fw-bold">Nyan Lin Htet @ Alex</span></p>
+      <p class="m-1">Read by Phyu Thant Kyi</p>
+      <p class="m-1">"စုတုပြု" Video Series, Episode 1</p>
+    </div>
+
+  </div>
+`;
 
 // button identifiers
 let aboveBeyondButton = document.querySelector(`#abovebeyond-button`);
 let steppingStoneButton = document.querySelector(`#steppingstone-button`);
 let kyaungshautButton = document.querySelector(`#kyaungshaut-button`);
+let sutupyuButton = document.querySelector(`#sutupyu-button`);
 
 //section identifier
 let eventsSection = document.querySelector(`#events-panel`);
@@ -812,7 +972,6 @@ function changeURLToAboveBeyond(event) {
         "/events.html#AboveBeyond",
     );
     eventsSection.innerHTML = aboveBeyondContent;
-
 }
 
 // SteppingStone button changes
@@ -831,7 +990,6 @@ function changeURLToSteppingStone(event) {
         "/events.html#SteppingStone",
     );
     eventsSection.innerHTML = steppingStoneContent;
-
 }
 
 // KyaungShaut button changes
@@ -870,10 +1028,8 @@ function changeURLToSuTuPyu(event) {
     eventsSection.innerHTML = suTuPyuContent;
 }
 
-
 // for clicking on the labels on the events page
 aboveBeyondButton.addEventListener(`click`, changeURLToAboveBeyond);
 steppingStoneButton.addEventListener(`click`, changeURLToSteppingStone);
 kyaungshautButton.addEventListener(`click`, changeURLToKyaungShaut);
 sutupyuButton.addEventListener(`click`, changeURLToSuTuPyu);
-
