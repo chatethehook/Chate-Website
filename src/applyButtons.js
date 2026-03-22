55737debc5c5adf97af2637470723b6ba8037d77
salamijason <shllsro@gmail.com>
2026-03-22 20:26:26 +0630

    Updated apply buttons - removed all buttons except global - added "MEXT ကို ချိတ်" button
    Signed-off-by: salamijason <shllsro@gmail.com>

--- src/applyButtons.js
+++ src/applyButtons.js
@@ -1,24 +1,12 @@
 let buttons = document.querySelector(`#apply-buttons`);
 
-let uwcLink =
-  "https://docs.google.com/forms/d/e/1FAIpQLSd8WKvfu84FiuBiL_gFvqC8dsp0lxWudqkr_9KRFNHPTK4TwA/viewform?usp=dialog";
-
 let globalLink =
   "https://docs.google.com/forms/d/e/1FAIpQLSf4pknYO2KCBWIgbzz2FowlWdgTNbnplDcVZgvetM3VgDMKvQ/viewform?usp=dialog";
 
-let lannSaLink = "https://forms.gle/MH4RNX73NZwHBK1j7";
-
-let transferLink = "https://forms.gle/NNVfb9xEXTZdBWei6";
+let mextLink = "https://forms.gle/W26T2d44TU73azEB7";
 
 let applyButtons = `
   <div >
-    <a
-    class="btn btn-primary accent-button m-1"
-    target="_blank"
-    href="${uwcLink}"
-    role="button"
-    >UWC</a
-    >
     <a
     class="btn btn-primary header-button m-1"
     target="_blank"
@@ -29,17 +17,11 @@ let applyButtons = `
     <a
     class="btn btn-primary accent-button m-1"
     target="_blank"
-    href="${lannSaLink}"
+    href="${mextLink}"
     role="button"
-    >Lann Sa</a
+    >MEXT ကို ချိတ်</a
     >
-    <a
-    class="btn btn-primary header-button m-1"
-    target="_blank"
-    href="${transferLink}"
-    role="button"
-    >Transfer</a
-    >
+
   </div>`;
 
 buttons.innerHTML = applyButtons;
