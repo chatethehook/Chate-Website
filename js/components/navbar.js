28a35c9ab9c5d7cd6222fd1d2e83935a7cf7d343
Ruben Htun <rubenhtun2003@gmail.com>
2026-08-16 10:49:22 +0700

    feat: change text icon to svg icon for arrow up right

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -73,7 +73,24 @@ function renderNavLinks(currentPath, mobile = false) {
       >
         ${
           mobile
-            ? '<span class="site-nav__arrow" aria-hidden="true">↗</span>'
+            ? `
+              <span class="site-nav__arrow" aria-hidden="true">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="1.8"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M7 7h10v10"></path>
+                  <path d="M7 17 17 7"></path>
+                </svg>
+              </span>
+            `
             : ""
         }
         <span class="site-nav__label">${label}</span>
