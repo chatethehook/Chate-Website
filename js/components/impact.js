7ded89d2b5d0fbf87acb052fa289af16fd956346
Ruben Htun <rubenhtun2003@gmail.com>
2026-08-20 16:47:27 +0700

    feat: added impact component

--- js/components/impact.js
+++ js/components/impact.js
@@ -4,72 +4,52 @@
  * @module components/impact
  */
 
+const STATS = [
+  { value: "$4M+", label: "Scholarships" },
+  { value: "32K+", label: "Followers" },
+  { value: "60+", label: "Volunteers" },
+  { value: "40+", label: "Webinars" },
+];
+
+function renderStat({ value, label }) {
+  return `
+    <div class="impact__stat">
+      <span class="impact__stat-value">${value}</span>
+      <span class="impact__stat-label">${label}</span>
+    </div>
+  `;
+}
+
 export function mountImpact() {
   const section = document.getElementById("impact");
   if (!section) return;
 
   section.innerHTML = `
-    <div class="container-fluid background-blue">
-      <div class="row m-0 p-2 justify-content-center">
-        <div class="justify-content-center p-0 pt-4 d-flex m-3" style="padding-right: 50px">
-          <h1 class="textwhite">An <span class="textwhite badge bg-warning text-wrap fs-2">Impactful</span> Organization</h1>
-        </div>
+    <div class="impact__container">
+      <h2 class="impact__heading">
+        An <span class="impact__highlight">Impactful</span> Organization
+      </h2>
 
-        <div class="col-12 col-md-6 mt-3 pt-4">
-          <iframe
-                        src="https://www.youtube.com/embed/u5jjnT3j17M?si=fm_GEqR6KZpiN68b"
-                        class="responsive-iframe"
-                        title="YouTube video player"
-                        frameborder="5"
-                        style="width: 100%; height: 400px"
-                        allow="
-                            accelerometer;
-                            autoplay;
-                            clipboard-write;
-                            encrypted-media;
-                            gyroscope;
-                            picture-in-picture;
-                            web-share;
-                        "
-                        referrerpolicy="strict-origin-when-cross-origin"
-                        allowfullscreen
-                    >
-                    </iframe>
-          <p class="text-center textwhite fw-bold mt-2">Interview with Voice of America (VOA) from August 2024</p>
-        </div>
-
-        <div class="col-12 col-md-6 p-2">
-          <div class="row g-3 justify-content-center">
-            <div class="col-sm m-3 text-center">
-              <div class="card gray-blue-gradient border-light shadow rounded-5">
-                <div class="card-body p-4">
-                  <h1 class="m-3 card-title textwhite fs-2"><span class="textorange fs-1">$4M+ </span><br>Scholarships</h1>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm m-3 text-center">
-              <div class="card gray-blue-gradient border-light shadow rounded-5">
-                <div class="card-body p-4">
-                  <h1 class="m-3 card-title textwhite fs-2"><span class="textorange fs-1">32K+</span><br>Followers</h1>
-                </div>
-              </div>
-            </div>
-            <div class="w-100"></div>
-            <div class="col-sm m-3 text-center">
-              <div class="card gray-blue-gradient border-light shadow rounded-5">
-                <div class="card-body p-4">
-                  <h1 class="m-3 card-title textwhite fs-2"><span class="textorange fs-1">60+</span><br>Volunteers</h1>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm m-3 text-center">
-              <div class="card gray-blue-gradient border-light shadow rounded-5">
-                <div class="card-body p-4">
-                  <h1 class="m-3 card-title textwhite fs-2"><span class="textorange fs-1">40+</span><br>Webinars</h1>
-                </div>
-              </div>
-            </div>
+      <div class="impact__grid">
+        <div class="impact__video">
+          <div class="impact__video-frame">
+            <iframe
+              src="https://www.youtube.com/embed/u5jjnT3j17M?si=fm_GEqR6KZpiN68b"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              loading="lazy"
+              allowfullscreen
+            ></iframe>
           </div>
+          <p class="impact__video-caption">
+            Interview with Voice of America (VOA) from August 2024
+          </p>
+        </div>
+
+        <div class="impact__stats">
+          ${STATS.map(renderStat).join("")}
         </div>
       </div>
     </div>
